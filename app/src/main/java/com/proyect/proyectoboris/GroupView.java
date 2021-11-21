72e5ad8e463b6f6a1399b3c13bfff3a7e18248e2
misael-reyes <misaelstark11@gmail.com>
2021-11-21 13:41:28 -0600

    se creo la vista ver grupo

---
+++ app/src/main/java/com/proyect/proyectoboris/GroupView.java
@@ -0,0 +1,14 @@
+package com.proyect.proyectoboris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_view);
+    }
+}
